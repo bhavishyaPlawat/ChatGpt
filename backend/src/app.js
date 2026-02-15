adb28896c74a41a8309defffe4bfb207178587da
bhavishya plawat <tobhavishya2004@gmail.com>
2026-02-15 22:53:39 +0530

    cors updated

--- backend/src/app.js
+++ backend/src/app.js
@@ -10,9 +10,10 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173", "https://chat-gpt-26.vercel.app"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
-  })
+  }),
 );
 
 /* using middlewares */
