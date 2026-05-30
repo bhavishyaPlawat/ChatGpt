302341f8ff5b69102906e3f8da8596e299461cd3
bhavishya plawat <tobhavishya2004@gmail.com>
2026-05-30 18:13:36 +0530

    Enable secure cookies for login token

--- backend/src/controller/auth.controller.js
+++ backend/src/controller/auth.controller.js
@@ -61,7 +61,7 @@ async function loginUser(req, res) {
 
   res.cookie("token", token, {
     httpOnly: true,
-    secure: false,
+    secure: true,
     sameSite: "None", // MUST be 'None' for cross-site
     maxAge: 24 * 60 * 60 * 1000, // 1 day
   });
